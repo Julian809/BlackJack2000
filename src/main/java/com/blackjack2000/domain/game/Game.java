29fe7a8354ef457c64330512a47958168fdfb017
Julian <Julian@10.44.207.159>
2018-06-18 15:31:08 -0700

    Added Game class

--- src/main/java/com/blackjack2000/domain/game/Game.java
+++ src/main/java/com/blackjack2000/domain/game/Game.java
@@ -2,10 +2,20 @@ package com.blackjack2000.domain.game;
 
 import com.blackjack2000.domain.Deck;
 import com.blackjack2000.domain.participant.Dealer;
+import com.blackjack2000.domain.participant.Player;
 
 public class Game {
     
-    Dealer dealer = new Dealer(new Deck());
-    Deal playerDeal = new Deal(dealer.dealCardFaceUp(), dealer.dealCardFaceUp());
-    Deal dealerDeal = new Deal(dealer.dealCardFaceUp(), dealer.dealCardFaceDown());
+    Dealer dealer;
+    Deal playerDeal;
+    Deal dealerDeal;
+    Player player;
+    
+    public Game(Dealer dealer, Player player) {
+    
+        dealer = new Dealer(new Deck());
+        playerDeal = new Deal(dealer.dealCardFaceUp(), dealer.dealCardFaceUp());
+        dealerDeal = new Deal(dealer.dealCardFaceUp(), dealer.dealCardFaceDown());
+        player = new Player(playerDeal);   
+    }
 }
