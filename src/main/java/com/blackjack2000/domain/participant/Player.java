1b9ee272b8d19a63e43d9e196ae3d975a26f2547
Julian <Julian@10.44.207.159>
2018-06-18 15:07:57 -0700

    Modified Player Class and Corrected sintax errors

--- src/main/java/com/blackjack2000/domain/participant/Player.java
+++ src/main/java/com/blackjack2000/domain/participant/Player.java
@@ -1,7 +1,25 @@
 package com.blackjack2000.domain.participant;
 
+import com.blackjack2000.domain.game.Deal;
+
 public class Player {
     
+    
+    private Deal deal;
+    public Player(Deal deal) {
+        
+        this.deal = deal;
+    }
+    
     //Place a bet
+    public void placeBet(int betAmount) {
+    
+    }
+    
+    public boolean aceIsWorth11() {
+        
+        return true;
+    }
+    
     
 }
